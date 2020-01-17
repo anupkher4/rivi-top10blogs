6c681383b7415d2a107d4c6afd1cb21a1b1d34db
Anup Kher <anup.kher.1990@gmail.com>
2020-01-17 10:10:57 +0530

    Initial Commit

---
+++ Top10BlogsTests/Top10BlogsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Top10BlogsTests.swift
+//  Top10BlogsTests
+//
+//  Created by Anup Kher on 17/01/20.
+//  Copyright © 2020 amprojects. All rights reserved.
+//
+
+import XCTest
+@testable import Top10Blogs
+
+class Top10BlogsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
